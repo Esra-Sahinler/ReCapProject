be1b2648df70fb83f50308e83c6ea4ac029b70f6
Esra-Sahinler <esrasahinler4@gmail.com>
2021-02-08 23:55:18 +0300

    9. gün ödev güncel

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,45 +1,45 @@
-﻿/*CREATE TABLE [dbo].[Brands]
-(
-	[BrandId] INT NOT NULL PRIMARY KEY, 
-    [BrandName] NVARCHAR(50) NULL
-)
+﻿CREATE TABLE [dbo].[Brands] (
+    [BrandId]   INT           IDENTITY (1, 1) NOT NULL,
+    [BrandName] NVARCHAR (50) NULL,
+    PRIMARY KEY CLUSTERED ([BrandId] ASC)
+);
 
-CREATE TABLE [dbo].[Colors]
-(
-	[ColorId] INT NOT NULL PRIMARY KEY, 
-    [ColorName] NVARCHAR(50) NULL
-)
+CREATE TABLE [dbo].[Colors] (
+    [ColorId]   INT           IDENTITY (1, 1) NOT NULL,
+    [ColorName] NVARCHAR (50) NULL,
+    PRIMARY KEY CLUSTERED ([ColorId] ASC)
+);
 
-CREATE TABLE [dbo].[Cars]
-(
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [BrandId] INT NULL,
-	[ColorId] INT NULL,
-	[ModelYear] INT,
-	[DailyPrice] DECIMAL,
-	[Description] NVARCHAR(50)
-	CONSTRAINT [FK_Cars_Colors] FOREIGN KEY ([ColorId]) REFERENCES [dbo].[Colors] ([ColorId]),
-	CONSTRAINT [FK_Cars_Brands] FOREIGN KEY ([BrandId]) REFERENCES [dbo].[Brands] ([BrandId])
-)*/
+CREATE TABLE [dbo].[Cars] (
+    [Id]        INT           IDENTITY (1, 1) NOT NULL,
+    [BrandId]      INT           NULL,
+    [ColorId]      INT           NULL,
+	[ModelYear]    NVARCHAR (50) NULL,
+    [DailyPrice]   DECIMAL (18)  NULL,
+    [Description] NVARCHAR (50) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    FOREIGN KEY ([ColorId]) REFERENCES [dbo].[Colors] ([ColorId]),
+    FOREIGN KEY ([BrandId]) REFERENCES [dbo].[Brands] ([BrandId])
+);
 
-INSERT INTO Cars(Id,BrandId,ColorId,ModelYear,DailyPrice,Description)
-VALUES 
-	(1,1,1,'2010','150','Manuel'),
-	(2,2,2,'2015','185','Manuel'),
-	(3,3,3,'2018','200','Otomatik'),
-	(4,3,2,'2019','350','Otomatik');
 
-INSERT INTO Colors(ColorId,ColorName) 
-VALUES
-	(1,'Beyaz'),
-	(2,'Siyah'),
-	(3,'Kırmızı');
-INSERT INTO Brands(BrandId,BrandName) 
-VALUES
-	(1,'Mercedes'),
-	(2,'BMW'),
-	(3,'Renault');
+INSERT INTO Colors(ColorName) VALUES
+	('Kırmızı'),
+	('Mavi'),
+	('Siyah');
 
-SELECT * FROM Cars
-SELECT * FROM Colors
-SELECT * FROM Brands
+INSERT INTO Brands(BrandName) VALUES
+	('Fiat'),
+	('Hyundai'),
+	('Nissan');
+	
+	
+INSERT INTO Cars(BrandId,ColorId,ModelYear,DailyPrice,Description) VALUES
+	('1','1','2013','130','Benzin'),
+	('1','2','2015','250','Dizel'),
+	('2','1','2017','400','Benzin'),
+	('2','2','2019','625','Dizel');
+
+    Select*From Cars
+    Select*From Colors
+    Select*From Brands
