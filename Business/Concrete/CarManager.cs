6c959093678e08e6616b43a942f94f3eb066a919
Esra-Sahinler <esrasahinler4@gmail.com>
2021-02-13 19:02:09 +0300

    10.gün ödev 1

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -1,4 +1,6 @@
 ﻿using Business.Abstract;
+using Business.Constants;
+using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
 using Entities.DTOs;
@@ -15,51 +17,54 @@ namespace Business.Concrete
         {
             _carDal = carDal;
         }
-        public void Add(Car car)
+        public IResult Add(Car car)
         {
             if (car.DailyPrice > 0 && car.Description.Length > 2)
             {
                 _carDal.Add(car);
+                return new SuccessResult(Messages.CarAdded);
             }
 
             else
             {
-                Console.WriteLine("Günlük fiyat 0'dan büyük tanımlama 2 karakterden uzun olmalıdır.");
+                return new ErrorResult(Messages.CarNameInvalid);
             }
         }
 
-        public void Delete(Car car)
+        public IResult Delete(Car car)
         {
             _carDal.Delete(car);
+            return new SuccessResult(Messages.CarDeleted);
         }
 
-        public List<Car> GetAll()
+        public IDataResult<List<Car>> GetAll()
         {
-            return _carDal.GetAll();
+            return new SuccessDataResult<List<Car>>( _carDal.GetAll(), Messages.CarAdded);
         }
 
-        public List<CarDetailDto> GetCarDetails()
+        public IDataResult<List<CarDetailDto>> GetCarDetails()
         {
-            return _carDal.GetCarDetails();
+            return new SuccessDataResult<List<CarDetailDto>>(_carDal.GetCarDetails());
         }
 
-        public List<Car> GetCarsByBrandId(int brandId)
+        public IDataResult<List<Car>> GetCarsByBrandId(int brandId)
         {
-            return _carDal.GetAll(c => c.BrandId == brandId);
+            return new SuccessDataResult<List<Car>>(_carDal.GetAll(c => c.BrandId == brandId));
         }
 
-        public List<Car> GetCarsByColorId(int colorId)
+        public IDataResult<List<Car>> GetCarsByColorId(int colorId)
         {
-            return _carDal.GetAll(c => c.ColorId == colorId);
+            return new SuccessDataResult<List<Car>>(_carDal.GetAll(c => c.ColorId == colorId));
         }
 
-        public void Update(Car car)
+        public IResult Update(Car car)
         {
             _carDal.Update(car);
+            return new SuccessResult(Messages.CarUpdated);
         }
-        public List<Car> GetById(int carId)
+        public IDataResult<Car> GetById(int carId)
         {
-            return _carDal.GetAll(c => c.Id == carId);
+            return new SuccessDataResult<Car>(_carDal.Get(c => c.Id == carId));
         }
     }
 }
--- Console/Program.cs
+++ Console/Program.cs
@@ -11,28 +11,28 @@ namespace ConsoleUI
         static void Main(string[] args)
         {
             CarTest();
-            ColorTest();
-            BrandTest();
+            //ColorTest();
+            //BrandTest();
         }
-        private static void ColorTest()
+        /*private static void ColorTest()
         {
             ColorManager colorManager = new ColorManager(new EfColorDal());
             colorManager.Add(new Color { ColorName = "Purple" });
-            colorManager.Delete(new Color { ColorId = 2 });
+            //colorManager.Delete(new Color { ColorId = 2 });
             colorManager.Update(new Color { ColorId = 3, ColorName = "Rolls Royce" });
             foreach (var color in colorManager.GetAll())
             {
                 Console.WriteLine(color.ColorName);
             }
 
-        }
+        }*/
         private static void CarTest()
         {
             CarManager carManager = new CarManager(new EfCarDal());
-            carManager.Add(new Car
+            var result = carManager.GetCarDetails();
+            /*carManager.Add(new Car
             {
                 BrandId = 1,
-                Id = 5,
                 ColorId = 2,
                 DailyPrice = 40000,
                 Description = "Honda",
@@ -41,8 +41,20 @@ namespace ConsoleUI
             carManager.Delete(new Car
             {
                 Id = 2
-            });
-            foreach (var car in carManager.GetAll())
+            });*/
+            if (result.Success == true)
+            {
+                foreach (var car in result.Data)
+                {
+                    Console.WriteLine(car.BrandName);
+                }
+            }
+            else
+            {
+                Console.WriteLine(result.Message);
+            }
+
+            /*foreach (var car in carManager.GetAll())
             {
                 Console.WriteLine(car.Id+" / "+car.Description);
             }
@@ -51,18 +63,19 @@ namespace ConsoleUI
             foreach (var car in carManager.GetCarDetails())
             {
                 Console.WriteLine(car.Id + " / " + car.BrandName + " / " + car.ColorName);
-            }
+            }*/
         }
-        private static void BrandTest()
+        /*private static void BrandTest()
         {
             BrandManager brandManager = new BrandManager(new EfBrandDalcs());
-            brandManager.Add(new Brand { BrandName = "Mercedes" });
-            brandManager.Delete(new Brand { BrandId = 2 });
-            brandManager.Update(new Brand { BrandId = 3, BrandName = "Rolls Royce" });
+            
+            //brandManager.Add(new Brand { BrandName = "Mercedes" });
+            //brandManager.Delete(new Brand { BrandId = 2 });
+            //brandManager.Update(new Brand { BrandId = 3, BrandName = "Rolls Royce" });
             foreach (var brand in brandManager.GetAll())
             {
                 Console.WriteLine(brand.BrandName);
             }
-        }
+        }*/
     }
 }
